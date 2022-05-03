f9afd6b97e98a81349491437572372b44836cdab
sreeprakashpk19 <prakash.candiser@gmail.com>
2022-05-03 16:30:15 +0530

    string

---
+++ src/java_string.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+@SuppressWarnings("unused")
+public class java_string {
+	public static void main(String[] args) {
+String str1="Test";
+String str2="Test";
+String  s1 =new String("Test");
+String  s2 =new String("Test");
+System.out.println("both are same"+(str1==str2));
+System.out.println("both are same"+(s1==s2));
+	}
+}
